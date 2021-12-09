8057f4e3e2d8ed38f056ddc50623167ade52c863
Konstantinos Rizos <konriz.sd@gmail.com>
2021-12-09 12:56:55 +0200

    Added Project and User Dtos

---
+++ FundRaiser.Common/Dto/ProjectUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using FundRaiser.Common.Models;
+using System;
+
+namespace FundRaiser.Common.Dto
+{
+    public class ProjectUpdateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public decimal? Goal { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public ProjectUpdateDto() { }
+
+        public ProjectUpdateDto(Project project)
+        {
+            Title = project.Title;
+            Description = project.Description;
+            Category = project.Category;
+            Goal = project.Goal;
+            EndDate = project.EndDate;
+        }
+    }
+}
---
+++ FundRaiser.Common/Dto/UserDto.cs
@@ -0,0 +1,51 @@
+﻿using FundRaiser.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FundRaiser.Common.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<ProjectDto> ProjectsAsCreator { get; set; } = new();
+        public List<ProjectDto> ProjectsAsBacker { get; set; } = new();
+
+        public UserDto() { }
+
+        public UserDto(User user)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+
+            
+            if (user.Projects != null)
+            {
+                var list = user.Projects
+                    .Where(p => p.UserId == Id)
+                    .ToList();
+                foreach (var l in list)
+                {
+                    ProjectsAsCreator.Add(new ProjectDto(l));
+                }
+            }
+
+            
+            if (user.Projects != null)
+            {
+                var lista = user.Funds
+                .Where(f => f.UserId == Id)
+                .Select(f => f.Reward.Project)
+                .Distinct()
+                .ToList();
+                foreach (var k in lista)
+                {
+                    ProjectsAsBacker.Add(new ProjectDto(k));
+                }
+            }
+        }
+    }
+}
